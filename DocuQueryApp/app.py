3015d9e949313d5c5620352d42751115354981a2
preetams7 <30720524+preetams7@users.noreply.github.com>
2025-06-02 22:30:50 +0530

    Add files via upload

---
+++ DocuQueryApp/app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import retrievalmodule 
+from retrievalmodule import DocumentRetrievalChain
+import os
+
+st.set_page_config(layout='wide')
+
+st.header("DocuQuery (concept)")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Upload a source PDF file to query", type=["pdf"])
+
+
+
+# Save file to desired location
+if uploaded_file is not None:
+    if uploaded_file.type == "application/pdf":
+        st.success("The uploaded file is a valid PDF.")
+        save_path = os.path.join("resources", uploaded_file.name)
+        with open(save_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        question = st.text_input("Enter your question here", key="question")
+        retrieval_chain_ob = DocumentRetrievalChain()
+        retrieval_chain = retrieval_chain_ob.get_chain()
+
+        if question:
+            with st.spinner("Thinking ...")
+            answer = retrieval_chain({"question":question}, return_only_outputs=True)
+            answer = answer['answer']
+            st.write(answer)
+    else:
+        st.error("Please upload a valid PDF file.")
+
+    
---
+++ DocuQueryApp/retrievalmodule.py
@@ -0,0 +1,41 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+class DocumentRetrievalChain:
+    def __init__(self):
+        for filename in os.listdir("resources"):
+            if filename.endswith(".pdf"):
+                loader = PyPDFLoader(f"resources/{filename}")
+                self.loaded_doc = loader.load()
+                break
+
+    def get_chunks(self):
+        splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", "\t"], chunk_size=400, chunk_overlap=50)
+        self.chunks = splitter.split_documents(self.loaded_doc)
+
+    def get_vectordb(self):
+        embeddings = HuggingFaceEmbeddings()
+        self.vectordb = FAISS.from_documents(self.chunks, embeddings)
+
+    def get_llm(self):
+        self.llm = OpenAI(temperature=0.4, max_tokens=100)
+
+    def get_chain(self):
+        self.get_chunks()
+        self.get_vectordb()
+        self.get_llm()
+        chain = RetrievalQAWithSourcesChain(llm=self.llm, retriever=self.vectordb.as_retriever(), chain_type="stuff")
+        return chain
+
+
+
+
+
