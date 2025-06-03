2279c82bf251b2a2baf7d2c69f4e1e577d49d6e7
preetams7 <30720524+preetams7@users.noreply.github.com>
2025-06-03 19:13:54 +0530

    Add files via upload

--- DocuQueryApp/retrievalmodule.py
+++ DocuQueryApp/retrievalmodule.py
@@ -5,6 +5,7 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain import OpenAI
+from langchain.chains.combine_documents import StuffDocumentsChain
 
 import os
 
@@ -23,13 +24,15 @@ class DocumentRetrievalChain:
         self.vectordb = FAISS.from_documents(self.chunks, embeddings)
 
     def get_llm(self):
-        self.llm = OpenAI(temperature=0.4, max_tokens=100)
+        
+        llm = OpenAI(temperature=0.4, max_tokens=100)
+        self.combine_chain = StuffDocumentsChain(llm=llm)
 
     def get_chain(self):
         self.get_chunks()
         self.get_vectordb()
         self.get_llm()
-        chain = RetrievalQAWithSourcesChain(llm=self.llm, retriever=self.vectordb.as_retriever(), chain_type="stuff")
+        chain = RetrievalQAWithSourcesChain(llm=self.combine_chain, retriever=self.vectordb.as_retriever(), chain_type="stuff")
         return chain
 
 
